380d1ba68e50b555c731f6afa73eaa8afb36bd4f
Dean Meacham <deanmeacham@rocketmail.com>
2019-04-26 15:08:29 -0400

    think we did it

--- global.js
+++ global.js
@@ -1,9 +1,28 @@
 parseDogs()
 
 function parseDogs() {
-    fetch('https://api.thedogapi.com/v1/images/search?limit=15')
-    .then((res) => res.json())
-    .then((dogs) => {
-        console.log(dogs)
-    })
+    fetch('https://raw.githubusercontent.com/gabe1331/dog-api/master/db.json')
+        .then((res) => res.json())
+        .then((dogs) => {
+            console.log(dogs)
+            dogs.forEach(doggo => {
+                let doggoDeets = `
+                    <div class="dog-card">
+                        <div class="dog-content">
+                            <div>
+                                <h3>${doggo.breed}</h3>
+                                <p>${doggo.height + " " + doggo.weight}</p>
+                                <div class="imgwrapper">
+                                    <img src=${doggo.image} alt="doggo">
+                                </div>
+                                <p>${doggo.lifespan}</p>
+                                <p>${doggo.bred_for}</p>
+                            </div>
+                        </div>
+                    </div>`
+
+                console.log(doggoDeets)
+                document.querySelector("#dogsWrapper").innerHTML += doggoDeets
+            })
+        })
 }
